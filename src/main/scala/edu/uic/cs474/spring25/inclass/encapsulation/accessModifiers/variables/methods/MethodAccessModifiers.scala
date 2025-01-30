ed37a56849d2af53f872b22411f3331f82eeff74
Austin Mordahl <amordahl@uic.edu>
2025-01-30 16:49:14 -0600

    Initial commit

---
+++ src/main/scala/edu/uic/cs474/spring25/inclass/encapsulation/accessModifiers/variables/methods/MethodAccessModifiers.scala
@@ -0,0 +1,6 @@
+package edu.uic.cs474.spring25.inclass.encapsulation.accessModifiers.variables.methods
+
+/**
+  * Like fields, methods can have protected and private modifiers, which mean the same things they do for data.
+  * Methods may also be declared 'final,' which indicate
+  */
